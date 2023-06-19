07a7b85dcc233ed77ebe6724bd45ab1bfd61d39d
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-06-19 13:21:22 +0200

    model save update

--- 05. Models/03.ANN_CostEstimator_For_LB/training_methods.py
+++ 05. Models/03.ANN_CostEstimator_For_LB/training_methods.py
@@ -102,7 +102,7 @@ def train_multiple_epochs(nb_epochs,model,training_loader,validation_loader,loss
         if avg_vloss < best_vloss:
             best_vloss = avg_vloss
             if save_trained:
-                min_val_model_path = 'trained_models/min_val/model_{}'.format(model_name)
+                min_val_model_path = 'trained_models/min_val/model_{}.pth'.format(model_name)
                 torch.save(model.state_dict(), min_val_model_path)
 
 
@@ -110,7 +110,7 @@ def train_multiple_epochs(nb_epochs,model,training_loader,validation_loader,loss
 
 
         epoch_number += 1
-    model_path = 'trained_models/all_epochs/model_{}_{}'.format(model_name, epoch_number)
+    model_path = 'trained_models/all_epochs/model_{}_{}.pth'.format(model_name, epoch_number)
     torch.save(model.state_dict(), model_path)
 
     return best_vloss,model_path,model
