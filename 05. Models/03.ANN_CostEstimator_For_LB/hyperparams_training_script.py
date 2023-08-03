9b0ae3ba7612ea06cdc5665bda817de1023c558f
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-08-03 15:35:39 +0200

    minor training script update
    Printing some stuff during execution and saving csv at intermediate times

--- 05. Models/03.ANN_CostEstimator_For_LB/hyperparams_training_script.py
+++ 05. Models/03.ANN_CostEstimator_For_LB/hyperparams_training_script.py
@@ -14,15 +14,14 @@ if __name__ == '__main__':
     period = "2030"
     folder = "../Data/RTS24_AC_12w"
     all_executions = DataLoading.list_executions(folder="../Data/RTS24_AC_12w", per=period, sc=sc)
-    len(all_executions)
     executions_start = 0
     executions_end = 40
     executions = all_executions[executions_start:executions_end]
     te_s = 0.3
     val_s = 0.4
 
-    exec_name = f"All_Exec_split_by_exec_penalize_test_bs_t{te_s}_v{val_s}"
-    folder_to_save = f"RTS24_AC_12w_split_by_exec_test_{executions_start}_{executions_end}"
+    exec_name = f"All_Exec_split_by_exec_t{te_s}_v{val_s}"
+    folder_to_save = f"RTS24_AC_12w_split_by_exec_{executions_start}_{executions_end}"
 
     #Load inputs and outputs in dataframes
     dfs_in, dfs_out = DataLoading.load_data(folder, executions, period, sc)
@@ -37,13 +36,13 @@ if __name__ == '__main__':
 
     #Perform the actual loop that checks multiple hyperparams
     i = 0
-    nbs_hidden = [3]
-    dors = [0]  # ,0.05,0.1]#,0.05]
+    nbs_hidden = [2,3]
+    dors = [0,0.05]  # ,0.05,0.1]#,0.05]
     relu_outs = [False]
 
-    batch_sizes = [32, 64, 128]
-    learning_rates = [0.0025 * 4 ** i for i in range(-1, 3, 1)]
-    nbs_e = [4, 8, 12, 16, 20]  # ,8]
+    batch_sizes = [32, 64, 128,256]
+    learning_rates = [0.0025 * 4 ** i for i in range(1, 4, 1)]
+    nbs_e = [4, 8, 16, 32, 64]  # ,8]
     negative_penalisations = [0]
 
     results = pd.DataFrame()
@@ -51,8 +50,12 @@ if __name__ == '__main__':
     hp_sets = ((nb_h, dor, relu_out, bs, lr, nb_e, np) for nb_h in nbs_hidden for dor in dors for relu_out in relu_outs
                for bs in batch_sizes for lr in learning_rates for nb_e in nbs_e for np in negative_penalisations)
 
+    print("Number of hyperparameter combinations to be considered:", len(list(hp_sets)),f" {len(nbs_e)} in the epochs dim")
+
     for hp_set in hp_sets:
+        print("Current set of hyperparameters:")
         print(hp_set)
+        print("Training starts")
         nb_hidden, dor, relu_out, bs, lr, nb_e, np = hp_set[0], hp_set[1], hp_set[2], hp_set[3], hp_set[4], hp_set[5], \
                                                      hp_set[6]
 
@@ -128,5 +131,6 @@ if __name__ == '__main__':
                              , index=[i])
             i += 1
             results = pd.concat([results, r])
+        results.to_csv(f"Loss_results_csv/{exec_name}.csv")
 
     results.to_csv(f"Loss_results_csv/{exec_name}.csv")
