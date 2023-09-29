60a68a96b794b9c2b1679225c142d2bd4789c243
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-09-29 22:38:22 +0200

    update 3-bus bystages folder I nc20

--- 05. Models/06.Benchmarking/I.Representative_days_based_on_Line_Benefit_NN_OC_fy_4/loop_3-bus.py
+++ 05. Models/06.Benchmarking/I.Representative_days_based_on_Line_Benefit_NN_OC_fy_4/loop_3-bus.py
@@ -6,9 +6,9 @@ path_to_scan = os.path.dirname(__file__)
 filter = '3-bus_ByStages_nc'
 
 directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if f.is_dir() and filter in os.path.basename(f.path)]
-print(directories)
+print(directories[2:4])
 
-for case in directories[]:
-    command = f"python oSN_Main_v2.py --case {CASE}"
+for case in directories[2:4]:
+    command = f"python oSN_Main_v2.py --case {case}"
     print(command)
-    #os.system(command)
+    os.system(command)
