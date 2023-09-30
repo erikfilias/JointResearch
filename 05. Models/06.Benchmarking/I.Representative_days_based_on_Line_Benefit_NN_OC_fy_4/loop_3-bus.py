fadbb6f9b6fc32d654081c2c43d81afeae696078
erikfilias <erikfilias@gmail.com>
2023-09-30 21:46:46 +0200

    Update loop_3-bus.py

--- 05. Models/06.Benchmarking/I.Representative_days_based_on_Line_Benefit_NN_OC_fy_4/loop_3-bus.py
+++ 05. Models/06.Benchmarking/I.Representative_days_based_on_Line_Benefit_NN_OC_fy_4/loop_3-bus.py
@@ -5,9 +5,9 @@ path_to_scan = os.path.dirname(__file__)
 filter = '3-bus_ByStages_nc'
 
 directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if f.is_dir() and filter in os.path.basename(f.path)]
-print(directories[2:4])
 
-for case in directories[2:4]:
+
+for case in directories:
     command = f"python oSN_Main_v2.py --case {case}"
     print(command)
     os.system(command)
