2da06e6c2cc84237c05f217b2d4c30b2815bef9b
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-10-04 11:46:17 +0200

    Update hpc submission files folder A

--- 05. Models/06.Benchmarking/A.The_full_year_MILP/sub_osn_main_DC_3-bus.sh
+++ 05. Models/06.Benchmarking/A.The_full_year_MILP/sub_osn_main_DC_3-bus.sh
@@ -6,14 +6,14 @@
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time="24:00:00"
+#SBATCH --time="36:00:00"
 #SBATCH --ntasks-per-node="36"
 #SBATCH --account="lp_elect_gen_modeling"
 #SBATCH --partition="batch"
     
 source activate Jr23
 echo "Activation OK"
-cd $VSC_DATA/JointResearch/05.\ Models/06.Benchmarking/A.The_full_year_MILP
+cd $VSC_SCRATCH/JointResearch/05.\ Models/06.Benchmarking/A.The_full_year_MILP
 
 echo "Starting runs"
 
--- 05. Models/06.Benchmarking/A.The_full_year_MILP/sub_osn_main_DC_RTS24.sh
+++ 05. Models/06.Benchmarking/A.The_full_year_MILP/sub_osn_main_DC_RTS24.sh
@@ -2,19 +2,20 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="3-bus_full_MILP"
+#SBATCH --job-name="RTS24_full_MILP_DC"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time="72:00:00"
+#SBATCH --time="36:00:00"
 #SBATCH --ntasks-per-node="36"
 #SBATCH --account="lp_elect_gen_modeling"
 #SBATCH --partition="batch"
-      
+    
 source activate Jr23
 echo "Activation OK"
-cd $VSC_DATA/JointResearch/05.Models/06.Benchmarking/A.The_full_year_MILP
+cd $VSC_SCRATCH/JointResearch/05.\ Models/06.Benchmarking/A.The_full_year_MILP
 
 echo "Starting runs"
 
-python oSN_Main_v2.py --case 3-bus
+python oSN_Main_DCOPF.py --case RTS24 
+
