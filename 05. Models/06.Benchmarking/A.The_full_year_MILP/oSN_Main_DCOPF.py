767b0b5cb9c72091a14dc3b909631dee3acf3436
Erik Alvarez <erikfilias@gmail.com>
2023-10-11 13:43:40 +0200

    Update oSN_Main_DCOPF.py

--- 05. Models/06.Benchmarking/A.The_full_year_MILP/oSN_Main_DCOPF.py
+++ 05. Models/06.Benchmarking/A.The_full_year_MILP/oSN_Main_DCOPF.py
@@ -1564,8 +1564,8 @@ def saving_results(DirName, CaseName, SolverName, model, optmodel):
     print('Writing           investment results  ... ', round(WritingInvResultsTime), 's')
 
     #%% outputting the generation cost
-    OutputResults = pd.Series(data=[model.pDiscountFactor[p]*model.pScenProb[p,sc]()*(optmodel.vTotalGCost[p,sc,n]()+optmodel.vTotalCCost[p,sc,n]()+optmodel.vTotalECost[p,sc,n]()+optmodel.vTotalRCost[p,sc,n]()) for p,sc,n in model.psn], index=pd.MultiIndex.from_tuples(model.psn))
-    OutputResults.to_frame(name='MEUR').rename_axis(['Period','Scenario','LoadLevel'], axis=0).reset_index().to_csv(_path+'/3.Out/oT_Result_GenerationCost_'+CaseName+'.csv', index=False, sep=',')
+    OutputResults = pd.Series(data=[model.pDiscountFactor[p]*model.pScenProb[p,sc]()*model.pLoadLevelDuration[n]()*(optmodel.vTotalGCost[p,sc,n]()+optmodel.vTotalCCost[p,sc,n]()+optmodel.vTotalECost[p,sc,n]()+optmodel.vTotalRCost[p,sc,n]())*1e3 for p,sc,n in model.psn], index=pd.MultiIndex.from_tuples(model.psn))
+    OutputResults.to_frame(name='mEUR').rename_axis(['Period','Scenario','LoadLevel'], axis=0).reset_index().to_csv(_path+'/3.Out/oT_Result_GenerationCost_'+CaseName+'.csv', index=False, sep=',')
 
     #%%  Power balance per period, scenario, and load level
     # incoming and outgoing lines (lin) (lout)
