43dce8ac8ea12d9b2acab82e1f6e426d86b15434
KristofPhillips95 <kristofphillips@gmail.com>
2023-11-07 13:34:53 +0100

    results 9n Folder E

---
+++ 05. Models/06.Benchmarking/E.Representative_days_based_on_Line_Benefits_OptModel/sub_loop_9n.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="9n_by_stages_loop_E"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="24:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/E.Representative_days_based_on_Line_Benefits_OptModel
+
+echo "Starting runs"
+
+python loop_by_stages.py --case 9n
