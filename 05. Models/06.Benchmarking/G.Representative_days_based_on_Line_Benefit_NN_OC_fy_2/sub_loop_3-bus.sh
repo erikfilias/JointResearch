91f32479c0d99d83d101275de86c42907d250545
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-11-08 21:34:11 +0100

    Results ByStages 3-bus folder G

--- 05. Models/06.Benchmarking/G.Representative_days_based_on_Line_Benefit_NN_OC_fy_2/sub_loop_3-bus.sh
+++ 05. Models/06.Benchmarking/G.Representative_days_based_on_Line_Benefit_NN_OC_fy_2/sub_loop_3-bus.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="3-bus_by_stages_loop_F"
+#SBATCH --job-name="3-bus_by_stages_loop_G"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -13,7 +13,7 @@
       
 source activate Jr23
 echo "Activation OK"
-cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/F.Representative_days_based_on_Line_Benefit_NN_OC_fy_1
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/G.Representative_days_based_on_Line_Benefit_NN_OC_fy_2
 
 echo "Starting runs"
 
