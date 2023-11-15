26fe9b85118f39190434e0860b8b0dbfe8fcc33d
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-11-15 10:34:56 +0100

    Update loop files

--- 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/loop_by_stages_highnb.py
+++ 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/loop_by_stages_highnb.py
@@ -10,12 +10,12 @@ case = args.case
 
 path_to_scan = os.path.dirname(__file__)
 filter = f'{case}_ByStages'
-filter_not = f"000"
+filter_high_nb = f"000"
 
-directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if f.is_dir() and filter and filter_not in os.path.basename(f.path)]
+directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if f.is_dir() and filter in os.path.basename(f.path) and filter_high_nb in os.path.basename(f.path)]
 
 
 for case in directories:
     command = f"python oSN_Main_DCOPF.py --case {case}"
     print(command)
-    os.system(command)
+    # os.system(command)
--- 05. Models/06.Benchmarking/G.Representative_days_based_on_Line_Benefit_NN_OC_fy_2/loop_by_stages.py
+++ 05. Models/06.Benchmarking/G.Representative_days_based_on_Line_Benefit_NN_OC_fy_2/loop_by_stages.py
@@ -7,15 +7,15 @@ parser.add_argument('--case',   type=str, default=None)
 args = parser.parse_args()
 
 case = args.case
-
+print(case)
 path_to_scan = os.path.dirname(__file__)
 filter = f'{case}_ByStages'
-filter_not = f"000"
+filter_high_nb = f"000"
 
-directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if f.is_dir() and filter and not(filter_not) in os.path.basename(f.path)]
-
+directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if f.is_dir() and filter in os.path.basename(f.path) and not(filter_high_nb in os.path.basename(f.path))]
+print(directories)
 
 for case in directories:
     command = f"python oSN_Main_DCOPF.py --case {case}"
     print(command)
-    os.system(command)
+    # os.system(command)
