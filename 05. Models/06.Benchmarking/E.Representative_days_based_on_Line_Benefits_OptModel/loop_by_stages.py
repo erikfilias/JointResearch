efc2c17ed52a3fbe09343306ef1e19efb3727a99
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-11-15 10:52:49 +0100

    update loop files by stages

--- 05. Models/06.Benchmarking/E.Representative_days_based_on_Line_Benefits_OptModel/loop_by_stages.py
+++ 05. Models/06.Benchmarking/E.Representative_days_based_on_Line_Benefits_OptModel/loop_by_stages.py
@@ -3,16 +3,26 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Introducing main parameters...')
 parser.add_argument('--case',   type=str, default=None)
+parser.add_argument('--highnb',   type=str, default=False)
 
 args = parser.parse_args()
 
 case = args.case
-print(case)
+highnb = args.highnb
+
 path_to_scan = os.path.dirname(__file__)
 filter = f'{case}_ByStages'
 filter_high_nb = f"000"
 
-directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if f.is_dir() and filter in os.path.basename(f.path) and not(filter_high_nb in os.path.basename(f.path))]
+if highnb:
+    directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if
+                   f.is_dir() and filter in os.path.basename(f.path) and (
+                               filter_high_nb in os.path.basename(f.path))]
+else:
+    directories = [os.path.basename(f.path) for f in os.scandir(path_to_scan) if
+                   f.is_dir() and filter in os.path.basename(f.path) and not (
+                               filter_high_nb in os.path.basename(f.path))]
+
 print(directories)
 
 for case in directories:
