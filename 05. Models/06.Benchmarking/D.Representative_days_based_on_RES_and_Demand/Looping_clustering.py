723c6fa33bcecdb453aba9317c4186b17d926129
Erik Alvarez <erikfilias@gmail.com>
2023-11-16 23:49:28 +0100

    Update Looping_clustering.py

--- 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/Looping_clustering.py
+++ 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/Looping_clustering.py
@@ -7,17 +7,7 @@ InitialTime = time.time()
 
 DirName  = os.getcwd()
 
-CaseName_Base = '3-bus'
-
-Folder_A = 'A.The_full_year_MILP'
-Folder_D = 'D.Representative_days_based_on_RES_and_Demand'
-Folder_E = 'E.Representative_days_based_on_Line_Benefits_OptModel'
-Folder_F = 'F.Representative_days_based_on_Line_Benefit_NN_OC_fy_1'
-Folder_G = 'G.Representative_days_based_on_Line_Benefit_NN_OC_fy_2'
-Folder_H = 'H.Representative_days_based_on_Line_Benefit_NN_OC_fy_3'
-Folder_I = 'I.Representative_days_based_on_Line_Benefit_NN_OC_fy_4'
-
-FoldersToPaste = [Folder_D, Folder_E, Folder_F, Folder_G, Folder_H, Folder_I]
+CaseName_Base = 'IEEE118'
 
 # Defining the case CaseName_ByStages plus the CaseName_ByStages_nc#
 CasesToPaste = []
