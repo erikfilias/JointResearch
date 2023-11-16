cb7f33220d8c6b12e9245bd666df30aa56ad2b2f
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-11-16 11:43:24 +0100

    submission files high nc folder D

--- 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/sub_loop_hnb_RTS24.sh
+++ 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/sub_loop_hnb_RTS24.sh
@@ -2,11 +2,11 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="9n_by_stages_loop_D_hnb"
+#SBATCH --job-name="RTS24_by_stages_loop_D_hnb"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time="10:00:00"
+#SBATCH --time="36:00:00"
 #SBATCH --ntasks-per-node="36"
 #SBATCH --account="lp_elect_gen_modeling"
 #SBATCH --partition="batch"
@@ -17,4 +17,4 @@ cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/D.Representative_days_ba
 
 echo "Starting runs"
 
-python loop_by_stages.py --case 9n --highnb True
+python loop_by_stages.py --case RTS24 --highnb True
