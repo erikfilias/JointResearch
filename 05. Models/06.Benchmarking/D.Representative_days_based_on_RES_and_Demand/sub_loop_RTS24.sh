c6513a9b37d9737c71aa9ad8a8097c533e967d8a
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-11-16 17:10:16 +0100

    Results folder D RTS24 ByStages

--- 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/sub_loop_RTS24.sh
+++ 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/sub_loop_RTS24.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time="1:00:00"
+#SBATCH --time="4:00:00"
 #SBATCH --ntasks-per-node="36"
 #SBATCH --account="lp_elect_gen_modeling"
 #SBATCH --partition="batch"
