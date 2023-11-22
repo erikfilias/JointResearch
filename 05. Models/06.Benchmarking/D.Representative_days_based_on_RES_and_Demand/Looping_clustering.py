2be8c52bd5fb4eafab35aa34931ad488234e7d4d
erikfilias <erikfilias@gmail.com>
2023-11-22 13:07:53 +0100

    Clustering Folder D and E IEEE118

--- 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/Looping_clustering.py
+++ 05. Models/06.Benchmarking/D.Representative_days_based_on_RES_and_Demand/Looping_clustering.py
@@ -13,7 +13,7 @@ CaseName_Base = 'IEEE118'
 CasesToPaste = []
 # CasesToPaste.append(CaseName_Base + '_ByStages')
 
-RangeClusters = [i for i in range(10, 101, 10)] + [i for i in range(150, 501, 50)] + [1000, 2000, 4000]
+RangeClusters = [i for i in range(10, 101, 10)] + [i for i in range(150, 251, 50)]
 
 for i in RangeClusters:
     CasesToPaste.append(CaseName_Base + '_ByStages_nc' + str(i))
