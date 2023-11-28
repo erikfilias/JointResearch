93885c7c81f61c5d834ce48159b8018141fc9272
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-11-28 14:39:33 +0100

    Folder J fix investment variables by copying

---
+++ 05. Models/06.Benchmarking/J.Full_year_operational_cost/CopyInvestmentDecisions.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import argparse
+import re
+
+#Specify the case, folder, and number of clusters
+#TODO: As parser arguments
+
+parser = argparse.ArgumentParser(description='Introducing main parameters...')
+parser.add_argument('--folder', type=str, default=None, help='Folder defining the clustering type')
+parser.add_argument('--subfolder', type=str, default=None, help='Case')
+
+args = parser.parse_args()
+
+folder = args.folder
+subfolder = args.subfolder
+
+# Define a regular expression pattern to extract case and nb_stages
+pattern = re.compile(r'(?P<case>.*?)_ByStages_nc(?P<nb_stages>.*)')
+
+# Use the pattern to match the subfolder string
+match = pattern.match(subfolder)
+
+# Extract values from the match
+if match:
+    case = match.group('case')
+    nb_stages = match.group('nb_stages')
+    print(f"Extracted case: {case}, nb_stages: {nb_stages}")
+else:
+    print("No match found.")
+
+# case = "3-bus"
+# nb_stages = 10
+# folder = "D.Representative_days_based_on_RES_and_Demand"
+
+#First read the relevant investment results from the ByStages run
+df_investment_results = pd.read_csv(
+    f"../{folder}/{case}_ByStages_nc{nb_stages}/3.Out/oT_Result_NetworkInvestment_{case}_ByStages_nc{nb_stages}.csv")
+
+# Then read the network parameters file that will be adjusted to perform the operational run
+
+# Specify the positions of the columns to be used as the index
+index_columns = [0, 1, 2]
+
+# Read the CSV file and set the specified columns as the index
+df_parameters = pd.read_csv(f"{case}/2.Par/oT_Data_Network_{case}.csv", index_col=index_columns)
+
+# Specify the names for the index columns, to have a coherent index with the network investment results
+index_names = ['InitialNode', 'FinalNode', 'Circuit']
+
+# Set the names for the index columns
+df_parameters.index.set_names(index_names, inplace=True)
+df_investment_results.set_index(index_names, inplace=True)
+
+#Add a sensitivity column
+df_investment_results["Sensitivity"] = "Yes"
+
+# Change the relevant columns in the parameter dataframe based on imported investment results from the ByStages run
+df_parameters["InvestmentFixed"] = df_investment_results["p.u."]
+df_parameters["Sensitivity"] = df_investment_results["Sensitivity"]
+
+#Finally, save the parameter file back to the original location, after altering the index names again to their original empty values
+df_parameters.index.names = [None] * len(df_parameters.index.names)
+df_parameters.to_csv(f"{case}/2.Par/oT_Data_Network_{case}.csv")
---
+++ 05. Models/06.Benchmarking/J.Full_year_operational_cost/loop_operational.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Introducing main parameters...')
+parser.add_argument('--case', type=str, default="3-bus")
+parser.add_argument('--min_nb', type=int, default=None, help='Minimum value for the integer following "nc"')
+parser.add_argument('--max_nb', type=int, default=None, help='Maximum value for the integer following "nc"')
+
+args = parser.parse_args()
+
+case = args.case
+min_nb = args.min_nb
+max_nb = args.max_nb
+
+path_to_scan = os.path.dirname("../D.Representative_days_based_on_RES_and_Demand/")
+
+filter = f'{case}_ByStages_nc'
+
+directories = []
+
+for f in os.scandir(path_to_scan):
+    if f.is_dir():
+        basename = os.path.basename(f.path)
+        match = re.search(f'{filter}(\d+)', basename)
+        if match:
+            nb = int(match.group(1))
+            if (min_nb is None or nb >= min_nb) and (max_nb is None or nb <= max_nb):
+                directories.append(basename)
+
+directories = sorted(directories, key=lambda x: int(re.search(r'nc(\d+)', x).group(1)))
+
+print(directories)
+
+folder = "D.Representative_days_based_on_RES_and_Demand"
+
+for subfolder in directories:
+    print(subfolder)
+    try:
+        command_fix_investments= f"python CopyInvestmentDecisions.py --folder {folder} --subfolder {subfolder} "
+        print(command_fix_investments)
+        os.system(command_fix_investments)
+    except Exception as e:
+        # Handle the exception
+        print(f"An error occurred: {e}")
+
+        # Terminate the program
+        sys.exit()
+
+
+    command_operational_run = f"python oSN_Main_operational.py --case {case}"
+    print(command_operational_run)
+    os.system(command_operational_run)
