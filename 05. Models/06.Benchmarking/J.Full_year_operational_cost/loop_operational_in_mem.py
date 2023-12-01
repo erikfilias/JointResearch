878be5cfc0fdc314e3a9135c5850b7eaa01f54c4
KristofPhillips95 <kristofphillips@gmail.com>
2023-12-01 11:27:19 +0100

    submission file RTS24 mod

--- 05. Models/06.Benchmarking/J.Full_year_operational_cost/loop_operational_in_mem.py
+++ 05. Models/06.Benchmarking/J.Full_year_operational_cost/loop_operational_in_mem.py
@@ -7,7 +7,10 @@ import argparse
 import re
 
 def parse_args():
-    folder_map = {"D":os.path.join("..","D.Representative_days_based_on_RES_and_Demand")}
+    folder_map = {"D":os.path.join("..","D.Representative_days_based_on_RES_and_Demand"),
+                  "E":os.path.join("..","E.Representative_days_based_on_Line_Benefits_OptModel"),
+                  "F":os.path.join("..","F.Representative_days_based_on_Line_Benefit_NN_OC_fy_1"),
+                  "G":os.path.join("..","G.Representative_days_based_on_Line_Benefit_NN_OC_fy_2"),}
     parser = argparse.ArgumentParser(description='Introducing main parameters...')
     parser.add_argument('--case', type=str, default="3-bus")
     parser.add_argument('--min_nb', type=int, default=None, help='Minimum value for the integer following "nc"')
