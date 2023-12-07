4554f4e51261d55974bc104a21ab7623c4fff652
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-12-07 22:47:04 +0100

    Submission files B and F

---
+++ 05. Models/06.Benchmarking/F.Representative_days_based_on_Line_Benefit_NN_OC_fy_1/sub_loop_RTS24_mod1_10-150.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="RTS24_mod1_by_stages_loop_B_1-150"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="36:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/F.Representative_days_based_on_Line_Benefit_NN_OC_fy_1
+
+echo "Starting runs"
+
+python loop_by_stages.py --case RTS24_mod1 --min_nb 10 --max_nb 150
+
