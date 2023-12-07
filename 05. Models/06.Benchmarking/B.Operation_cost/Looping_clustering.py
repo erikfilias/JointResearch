c3afff109ee22f2e5317b9345d033b62ab96fa6b
erikfilias <erikfilias@gmail.com>
2023-12-07 18:34:22 +0100

    Clustering RTS24_mod1 cases in Folder B

--- 05. Models/06.Benchmarking/B.Operation_cost/Looping_clustering.py
+++ 05. Models/06.Benchmarking/B.Operation_cost/Looping_clustering.py
@@ -7,7 +7,7 @@ InitialTime = time.time()
 
 DirName  = os.getcwd()
 
-CaseName_Base = 'IEEE118'
+CaseName_Base = 'RTS24_mod1'
 
 # Defining the case CaseName_ByStages plus the CaseName_ByStages_nc#
 CasesToPaste = []
