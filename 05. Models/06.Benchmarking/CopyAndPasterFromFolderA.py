6100a450d6603f1d5d97443bc2d5fcd14d05b779
erikfilias <erikfilias@gmail.com>
2023-12-08 11:00:59 +0100

    Clustering RTS24_mod1 Folder E

--- 05. Models/06.Benchmarking/CopyAndPasterFromFolderA.py
+++ 05. Models/06.Benchmarking/CopyAndPasterFromFolderA.py
@@ -6,7 +6,7 @@ InitialTime = time.time()
 
 DirName  = os.getcwd()
 
-CaseName_Base = 'IEEE118'
+CaseName_Base = 'RTS24_mod1'
 
 Folder_A = 'A.The_full_year_MILP'
 Folder_B = 'B.Operation_cost'
@@ -18,7 +18,7 @@ Folder_H = 'H.Representative_days_based_on_Line_Benefit_NN_OC_fy_3'
 Folder_I = 'I.Representative_days_based_on_Line_Benefit_NN_OC_fy_4'
 
 # FoldersToPaste = [Folder_D, Folder_E, Folder_F, Folder_G, Folder_H, Folder_I]
-FoldersToPaste = [Folder_B]
+FoldersToPaste = [Folder_F]
 
 # Defining the case CaseName_ByStages plus the CaseName_ByStages_nc#
 CasesToPaste = []
