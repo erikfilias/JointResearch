bdb6f4752e2071370be2a4b3c099117f440f7079
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-12-15 12:24:00 +0100

    Update HPC files folder L and remove logs

--- 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_loop_investments_3-bus.sh
+++ 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_loop_investments_3-bus.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="3-bus_IPH"
+#SBATCH --job-name="3-bus_IPHC"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -13,7 +13,7 @@
       
 source activate Jr23
 echo "Activation OK"
-cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/K.Investments_per_hour
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/L.Cont_Investments_per_hour
 
 echo "Starting runs"
 
