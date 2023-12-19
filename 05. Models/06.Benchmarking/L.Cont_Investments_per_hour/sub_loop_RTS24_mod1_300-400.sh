f6a5e8882ae92e4f483459c49436a2e323bcdc38
KristofPhillips95 <kristofphillips@gmail.com>
2023-12-19 11:13:54 +0100

    Results RTS24_mod1 folder L
    Update with other clustering seed

--- 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_loop_RTS24_mod1_300-400.sh
+++ 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_loop_RTS24_mod1_300-400.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="RTS24_mod1_by_stages_loop_L_250-400"
+#SBATCH --job-name="RTS24_mod1_by_stages_loop_L_300-400"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -17,4 +17,4 @@ cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/L.Cont_Investments_per_h
 
 echo "Starting runs"
 
-python loop_by_stages.py --case RTS24_mod1 --min_nb 250 --max_nb 400
+python loop_by_stages.py --case RTS24_mod1 --min_nb 300 --max_nb 400
