5c0436f5dcbbb3ddc6e4830aa2ae2f36bc31500b
KristofPhillips95 <kristofphillips@gmail.com>
2023-12-22 13:23:04 +0100

    Add hpc submission scripts IEEE folder L Bystages

---
+++ 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_loop_IEEE118_150-200.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="IEEE118_by_stages_loop_L_150-200"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="6:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/L.Cont_Investments_per_hour
+
+echo "Starting runs"
+
+python loop_by_stages.py --case IEEE118 --min_nb 150 --max_nb 250
