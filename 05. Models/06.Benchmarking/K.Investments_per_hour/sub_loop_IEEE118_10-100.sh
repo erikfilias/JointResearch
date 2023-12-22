03cf460482567a46fe0c9e207e620e9c0767bfbe
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-12-22 16:43:57 +0100

    Hourly discrete investments IEEE1118: hpc submission file

--- 05. Models/06.Benchmarking/K.Investments_per_hour/sub_loop_IEEE118_10-100.sh
+++ 05. Models/06.Benchmarking/K.Investments_per_hour/sub_loop_IEEE118_10-100.sh
@@ -2,11 +2,11 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="IEEE118_IPHC"
+#SBATCH --job-name="RTS24_mod1_by_stages_loop_K_10-250"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time="36:00:00"
+#SBATCH --time="6:00:00"
 #SBATCH --ntasks-per-node="36"
 #SBATCH --account="lp_elect_gen_modeling"
 #SBATCH --partition="batch"
@@ -17,4 +17,4 @@ cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/K.Investments_per_hour
 
 echo "Starting runs"
 
-python loop_hourly_investments.py --case IEEE118
+python loop_by_stages.py --case RTS24_mod1 --min_nb 10 --max_nb 250
