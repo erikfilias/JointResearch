4463c7f2ee3de32552945ba0d4545926272236c4
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-12-22 16:48:03 +0100

    hpc submission files IEEE118 folder K

--- 05. Models/06.Benchmarking/K.Investments_per_hour/sub_loop_IEEE118_150-200.sh
+++ 05. Models/06.Benchmarking/K.Investments_per_hour/sub_loop_IEEE118_150-200.sh
@@ -2,11 +2,11 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="RTS24_mod1_by_stages_loop_K_10-100"
+#SBATCH --job-name="RTS24_mod1_by_stages_loop_K_150-200"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time="6:00:00"
+#SBATCH --time="72:00:00"
 #SBATCH --ntasks-per-node="36"
 #SBATCH --account="lp_elect_gen_modeling"
 #SBATCH --partition="batch"
@@ -17,4 +17,4 @@ cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/K.Investments_per_hour
 
 echo "Starting runs"
 
-python loop_by_stages.py --case RTS24_mod1 --min_nb 10 --max_nb 100
+python loop_by_stages.py --case RTS24_mod1 --min_nb 150 --max_nb 200
