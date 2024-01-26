9f7bae552a23add9e644b886f1eafab52f24c49a
Kristof Phillips <Kristof.phillips@kuleuven.be>
2024-01-26 11:36:50 +0100

    Folder J FYOP IEEE118 1-100 D,E

--- 05. Models/06.Benchmarking/J.Full_year_operational_cost/sub_loop_118_E_10-100.sh
+++ 05. Models/06.Benchmarking/J.Full_year_operational_cost/sub_loop_118_E_10-100.sh
@@ -2,11 +2,11 @@
 
 
 #SBATCH --cluster="genius"
-#SBATCH --job-name="118_FYOP_loop_E"
+#SBATCH --job-name="118_FYOP_loop_E_10-100"
 #SBATCH --nodes="1"
 #SBATCH --mail-user="kristof.phillips@kuleuven.be"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time="12:00:00"
+#SBATCH --time="36:00:00"
 #SBATCH --ntasks-per-node="36"
 #SBATCH --account="lp_elect_gen_modeling"
 #SBATCH --partition="batch"
@@ -17,4 +17,4 @@ cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/J.Full_year_operational_
 
 echo "Starting runs"
 
-python loop_operational_in_mem.py --case IEEE118 --origin_folder E --min_nb 10 --max_nb 150
+python loop_operational_in_mem.py --case IEEE118 --origin_folder E --min_nb 10 --max_nb 100
