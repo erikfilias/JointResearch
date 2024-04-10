a765eb428eea3ace9670e2a9800d92c6e352f295
erikfilias <erikfilias@gmail.com>
2024-04-10 22:36:50 +0200

    Clustering Folder B RTS24_mood1

--- 05. Models/06.Benchmarking/B.Operation_cost/Looping_clustering.py
+++ 05. Models/06.Benchmarking/B.Operation_cost/Looping_clustering.py
@@ -7,7 +7,7 @@ InitialTime = time.time()
 
 DirName  = os.getcwd()
 
-CaseName_Base = 'RTS24'
+CaseName_Base = 'RTS24_mod1'
 
 # Defining the case CaseName_ByStages plus the CaseName_ByStages_nc#
 CasesToPaste = []
