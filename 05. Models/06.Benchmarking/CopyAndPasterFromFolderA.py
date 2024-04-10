2ee3f49deee03f2969cf97197ed9181d0825e553
erikfilias <erikfilias@gmail.com>
2024-04-10 12:12:54 +0200

    Results ByStages_nc200 IEEE118_mod1 Folder L

--- 05. Models/06.Benchmarking/CopyAndPasterFromFolderA.py
+++ 05. Models/06.Benchmarking/CopyAndPasterFromFolderA.py
@@ -19,8 +19,8 @@ Folder_I = 'I.Representative_days_based_on_Line_Benefit_NN_OC_fy_4'
 Folder_K = 'K.Investments_per_hour'
 Folder_L = 'L.Cont_Investments_per_hour'
 
-# FoldersToPaste = [Folder_D, Folder_E, Folder_F, Folder_G, Folder_H, Folder_I]
-FoldersToPaste = [Folder_L]
+FoldersToPaste = [Folder_B, Folder_D, Folder_E, Folder_F, Folder_G, Folder_H, Folder_I]
+# FoldersToPaste = [Folder_L]
 
 # Defining the case CaseName_ByStages plus the CaseName_ByStages_nc#
 CasesToPaste = []
