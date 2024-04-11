e37ef50d56b36ebbf02fd785d539d52c1921daa5
KristofPhillips95 <kristofphillips@gmail.com>
2024-04-11 11:11:02 +0200

    hpc files and case overview

---
+++ 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_loop_investments_RTS24.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="RTS24_mod1_IPHC"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="24:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/L.Cont_Investments_per_hour
+
+echo "Starting runs"
+
+python loop_hourly_investments.py --case RTS24
