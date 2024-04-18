05930b2d2e7f186ae3551cd0d2486ab6550fba43
KristofPhillips95 <kristofphillips@gmail.com>
2024-04-18 11:12:54 +0200

    Results Folder J 9n & RTS24_mod1

--- 05. Models/06.Benchmarking/J.Full_year_operational_cost/sub_loop_9n_All.sh
+++ 05. Models/06.Benchmarking/J.Full_year_operational_cost/sub_loop_9n_All.sh
@@ -22,5 +22,5 @@ origin_folders=("B" "D" "E" "K" "L")
 # Loop over each origin folder
 for origin_folder in "${origin_folders[@]}"; do
     echo "Running for origin_folder: $origin_folder"
-    python loop_operational_in_mem.py --case RTS24 --origin_folder "$origin_folder" --min_nb 10 --max_nb 400
+    python loop_operational_in_mem.py --case 9n --origin_folder "$origin_folder" --min_nb 10 --max_nb 400
 done
