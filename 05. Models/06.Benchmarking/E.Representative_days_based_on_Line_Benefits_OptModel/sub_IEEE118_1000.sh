1cc7916a0c32a66102d5404caa6e0beb1bf9a318
KristofPhillips95 <kristofphillips@gmail.com>
2024-04-30 13:35:04 +0200

    hpc submission files nc1000

---
+++ 05. Models/06.Benchmarking/E.Representative_days_based_on_Line_Benefits_OptModel/sub_IEEE118_1000.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="118_BS_E_1000"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="72:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/E.Representative_days_based_on_Line_Benefits_OptModel
+
+echo "Starting runs"
+
+
+python loop_by_stages.py --case IEEE118 --min_nb 1000 --max_nb 1000
---
+++ 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_RTS24_1000.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="RTS24_by_stages_L_1000"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="72:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/L.Cont_Investments_per_hour
+
+echo "Starting runs"
+
+python loop_by_stages.py --case RTS24 --min_nb 1000 --max_nb 1000
+
