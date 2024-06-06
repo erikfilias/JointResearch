a1987914d5dc8b372ca398e72940fcf455a98e6d
KristofPhillips95 <kristofphillips@gmail.com>
2024-06-06 11:56:30 +0200

    Results 9n_mod1 folder A

---
+++ 05. Models/06.Benchmarking/A.The_full_year_MILP/sub_osn_main_DC_9n_mod1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="9n_mod1_full_MILP_DC"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="24:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+    
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/A.The_full_year_MILP
+
+echo "Starting runs"
+
+python oSN_Main_DCOPF.py --case 9n_mod1
+
