f8477975ced0d20db0a57095caa3eec5c01f2d99
KristofPhillips95 <kristofphillips@gmail.com>
2024-06-20 15:41:46 +0200

    HPC submission files folder K RTS24 mod2

---
+++ 05. Models/06.Benchmarking/J.Full_year_operational_cost/sub_loop_RTS24_mod2_B.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="RTS24_mod2_FYOP_loop_B"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="10:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/J.Full_year_operational_cost
+
+echo "Starting runs"
+
+python loop_operational_in_mem.py --case RTS24_mod2 --origin_folder B --min_nb 10 --max_nb 400
