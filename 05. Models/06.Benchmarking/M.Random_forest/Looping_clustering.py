860223c61c70e926c019534bd7841fd5dcfbf117
erikfilias <erikfilias@gmail.com>
2024-07-19 17:49:02 +0200

    Create Looping_clustering.py

---
+++ 05. Models/06.Benchmarking/M.Random_forest/Looping_clustering.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import time
+import OutputBasedClustering_Modules as obc
+
+InitialTime = time.time()
+
+DirName  = os.getcwd()
+
+CaseName_Base = 'RTS24_mod2'
+
+# Defining the case CaseName_ByStages plus the CaseName_ByStages_nc#
+CasesToPaste = []
+# CasesToPaste.append(CaseName_Base + '_ByStages')
+
+RangeClusters = [i for i in range(10, 101, 10)] + [i for i in range(150, 401, 50)]
+# RangeClusters = [1000]
+
+for i in RangeClusters:
+    CasesToPaste.append(CaseName_Base + '_ByStages_nc' + str(i))
+
+CasesTime = time.time() - InitialTime
+StartTime = time.time()
+print('The time for defining the cases is ' + str(CasesTime) + ' seconds')
+
+for i in RangeClusters:
+    print('Clustering of case ' + CaseName_Base + '_ByStages_nc' + str(i) + ' is starting')
+    obc.main(0, DirName, i, CaseName_Base)
+    ClusteringTime = time.time() - StartTime
+    StartTime = time.time()
+    print('The time for clustering the case ' + CaseName_Base + '_ByStages_nc' + str(i) + ' is ' + str(ClusteringTime) + ' seconds')
+
+elapsed_time = round(time.time() - InitialTime)
+print('Elapsed time: {} seconds'.format(elapsed_time))
