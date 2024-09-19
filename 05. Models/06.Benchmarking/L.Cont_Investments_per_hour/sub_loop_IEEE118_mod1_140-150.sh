3f12ea8c96df55111d7669b5a5ed3b36c4780aae
KristofPhillips95 <kristofphillips@gmail.com>
2024-09-19 22:43:55 +0200

    Results Bystages IEEE118_mod1 Folder L

---
+++ 05. Models/06.Benchmarking/L.Cont_Investments_per_hour/sub_loop_IEEE118_mod1_140-150.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="118_by_stages_loop_L_140-150"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="72:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/L.Cont_Investments_per_hour
+
+echo "Starting runs"
+
+python loop_by_stages.py --case IEEE118_mod1 --min_nb 140 --max_nb 150
