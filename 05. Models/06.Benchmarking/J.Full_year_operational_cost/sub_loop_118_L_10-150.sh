30a4123a418e5ee71f421bf2647d6dbc964b9dd9
KristofPhillips95 <kristofphillips@gmail.com>
2024-09-23 18:04:04 +0200

    Results IEEE118 folder J (FYOP), folder B,D,E,K,L

---
+++ 05. Models/06.Benchmarking/J.Full_year_operational_cost/sub_loop_118_L_10-150.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+
+#SBATCH --cluster="genius"
+#SBATCH --job-name="118_FYOP_loop_L_10-150"
+#SBATCH --nodes="1"
+#SBATCH --mail-user="kristof.phillips@kuleuven.be"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time="36:00:00"
+#SBATCH --ntasks-per-node="36"
+#SBATCH --account="lp_elect_gen_modeling"
+#SBATCH --partition="batch"
+      
+source activate Jr23
+echo "Activation OK"
+cd $VSC_SCRATCH/JointResearch/05.Models/06.Benchmarking/J.Full_year_operational_cost
+
+echo "Starting runs"
+
+python loop_operational_in_mem.py --case IEEE118 --origin_folder L --min_nb 10 --max_nb 150
